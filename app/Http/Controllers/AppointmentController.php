2ceccb142afbe7af50c2fef37469fdd184d10783
Agustin <agustin.e.blanco@gmail.com>
2020-03-26 05:03:19 -0300

    Send FCM to User Fix Cancel Confirmed-Appointment

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -109,7 +109,7 @@ class AppointmentController extends Controller
         if($request->has('justification')){
             $cancellation = new CancelledAppointment();
             $cancellation->justification = $request->input('justification');
-            $cancellation->cancelled_by = auth()->id();
+            $cancellation->cancelled_by_id = auth()->id();
 
             $appointment->cancellation()->save($cancellation);
         }
